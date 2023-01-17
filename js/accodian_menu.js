5e23393dd6fc107af94644e4b13e59ee2a275d2c
wonhj <wonhjteacher@gmail.com>
2023-01-17 12:03:41 +0900

    아코디언메뉴

---
+++ js/accodian_menu.js
@@ -0,0 +1,20 @@
+const listAll = document.querySelector('.main') ;
+const list = document.querySelectorAll('.main>li') ;
+const sub = document.querySelectorAll('.sub');
+listAll.addEventListener('click',(e)=>{
+   let isState= e.target.classList.contains('on') ;
+   if(isState){ // li.on클래스가 있는 경우 
+     e.target.classList.remove('on')
+     e.target.childNodes.forEach(ele=>{
+        if(ele.tagName === 'UL'){
+          ele.classList.remove('open')
+        }
+     })
+   }else{ //li 클래스가 없는 경우  
+     list.forEach(ele=>ele.classList.remove('on'))
+     e.target.classList.add('on')
+     sub.forEach(ele=>ele.classList.remove('open'))
+     let index = e.target.dataset.num;
+     sub[index].classList.add('open')
+   }
+})
